2245eb59061f915abee4dee4641ff759892523dd
UlrikeWerner <ulrike_werner@web.de>
2023-08-29 15:34:57 +0200

    create class Book

--- src/main/java/de/bookshop/classes/Book.java
+++ src/main/java/de/bookshop/classes/Book.java
@@ -1,4 +1,35 @@
 package de.bookshop.classes;
 
 public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getIsbn(){
+        return isbn;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
